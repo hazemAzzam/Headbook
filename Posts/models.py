7fea39fb0e6c3f40d0b0a7b06791eb13f8e5bb47
hazemAzzam <61450444+hazemAzzam@users.noreply.github.com>
2022-09-22 00:02:09 +0200

    Delete Posts directory

--- Posts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth import get_user_model
-# Create your models here.
-class Post(models.Model):
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    content = models.TextField(max_length=250)
-    pub_date=models.DateTimeField(auto_now_add=True, blank=True)
-    
